c3ba3bc554e2a909334bc88b719af5681c40c8bf
CapMorgan1987 <aron.morgan87@gmail.com>
2021-05-11 18:38:34 +0200

    added components for Breadcrumbs, Header and Nav

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-breadcrumbs :items="items"></v-breadcrumbs>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Breadcrumbs",
+
+    data() {
+      return {
+        items: [{ text: "Ankete" }],
+      };
+    },
+  };
+</script>
+
+<style>
+</style>
