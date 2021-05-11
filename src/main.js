113cbea906ab25485d28e8a7dec8c58c6dbbd58b
CapMorgan1987 <aron.morgan87@gmail.com>
2021-05-11 17:05:26 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  VueRouter,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
